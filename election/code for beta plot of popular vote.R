320e4cd094c89de6c0f0a8065a022b1ddead0f7c
supi <jam4plays@gmail.com>
2023-11-15 09:15:13 +0100

    updated for 2023-11-15

---
+++ election/code for beta plot of popular vote.R
@@ -0,0 +1,41 @@
+library(data.table)
+library(ggplot2)
+
+data <- data.table(p=c(0:20)/20)
+w = 12.64466	
+l =	15.43882
+
+
+
+
+data$density <- data$p**w*(1-data$p)**l# * factorial(w+l+1)/(factorial(w)*factorial(l))
+data$density <- data$density / sum(data$density)
+data$category <- fifelse(data$p < 0.5, "Trump Win More Likely", "Biden Win More Likely")
+data$category <- as.character(data$category)
+
+# max value
+# data[order(-ways)][1,1]
+
+# plotting
+ggplot(data, aes(x=as.integer(p*100), y=density))+
+  geom_col(aes(fill=category), size=1.5)+
+  scale_fill_manual(values=c(
+    "Trump Win More Likely"="#ee4444",
+    "Biden Win More Likely"="#0077ee"
+  ))+
+  #geom_vline(xintercept = as.numeric(data[order(-density)][1,1]), linetype="dotted", color="#666666", size=1)+
+  geom_vline(xintercept = 50, color="#000000", size=1, linetype="dotted")+
+  theme_bw()+
+  xlab("Probability that Biden Wins Popular Vote")+
+  ylab("Relative Density of that Probability being true")+
+  theme(legend.position = "bottom")+
+  scale_x_continuous(breaks=seq(0,100,5),expand = c(0,0), limits=c(0,101))+
+  scale_y_continuous(expand=c(0,0), limits=c(0,0.25))+
+  theme(axis.line = element_line(color='black'),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank())+
+  labs(title="Popular Vote", subtitles="As of 2023-11-15")
+
+ggsave("popular vote 2023-11-15.png")
---
+++ election/code for beta plot of states.R
@@ -0,0 +1,66 @@
+library(data.table)
+library(ggplot2)
+
+
+state_plots <- function(state, date, data, w, l){
+
+  data$density <- data$p**w*(1-data$p)**l# * factorial(w+l+1)/(factorial(w)*factorial(l))
+  data$density <- data$density / sum(data$density)
+  data$category <- fifelse(data$p < 0.5, "Trump Win More Likely", "Biden Win More Likely")
+  data$category <- as.character(data$category)
+
+  # max value
+  # data[order(-ways)][1,1]
+
+
+  # plotting
+  g <- ggplot(data, aes(x=as.integer(p*100), y=density))+
+  geom_col(aes(fill=category), size=1.5)+
+  scale_fill_manual(values=c(
+    "Trump Win More Likely"="#ee4444",
+    "Biden Win More Likely"="#0077ee"
+  ))+
+  #geom_vline(xintercept = as.numeric(data[order(-density)][1,1]), linetype="dotted", color="#666666", size=1)+
+  geom_vline(xintercept = 50, color="#000000", size=1, linetype="dotted")+
+  theme_bw()+
+  xlab("Probability that Biden Wins Popular Vote")+
+  ylab("Relative Density of that Probability being true")+
+  theme(legend.position = "bottom")+
+  scale_x_continuous(breaks=seq(0,100,5),expand = c(0,0), limits=c(0,101))+
+  scale_y_continuous(expand=c(0,0), limits=c(0,0.25))+
+  theme(axis.line = element_line(color='black'),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank())+
+  labs(title=state, subtitles=date)
+  #ggsave(filename = paste(state, date,'png'), g)
+  g
+}
+
+date <- "2023-11-15"
+data <- data.table(p=c(0:20)/20)
+
+state_plots(       "Arizona", date, data, 0.92, 4.51)
+ggsave(paste("Arizona", date, ".png"))
+
+state_plots(       "Georgia", date, data, 0.00, 6.98)
+ggsave(paste("Georgia", date, ".png"))
+
+state_plots(       "Florida", date, data, 0.00, 0.45)
+ggsave(paste("Florida", date, ".png"))
+
+state_plots(     "Michigan", date, data, 3.84, 1.26)
+ggsave(paste("Michigan", date, ".png"))
+
+state_plots(        "Nevada", date, data, 0.59, 4.45)
+ggsave(paste("Nevada", date, ".png"))
+
+state_plots("North Carolina", date, data, 0.50, 1.87)
+ggsave(paste("North Carolina", date, ".png"))
+
+state_plots(  "Pennsylvania", date, data, 1.40, 3.76)
+ggsave(paste("Pennsylvania", date, ".png"))
+
+state_plots(     "Wisconsin", date, data, 3.13, 1.83)
+ggsave(paste("Wisconsin", date, ".png"))
