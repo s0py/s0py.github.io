f700b967b138f84a753ac235cffdcba1a3c5d1ce
supi <jam4plays@gmail.com>
2025-02-15 16:39:37 +0100

    scaled down large images and decreased quality to save bandwidth

--- coc/crimson_letters/towebp.py
+++ coc/crimson_letters/towebp.py
@@ -1,17 +1,9 @@
 import os
 from PIL import Image
-import random
 
-# Get all image files in current directory
-image_extensions = ('.png')
-image_files = [f for f in os.listdir('.') if f.lower().endswith(image_extensions)]
-
-for image_file in image_files:
-    print(f"Processing: {image_file}")
-    # Open and rotate image
-    with Image.open(image_file) as img:
-        # Save with '_rotated' suffix
-        filename, ext = os.path.splitext(image_file)
-        new_filename = f"{filename}.webp"
-        img.save(new_filename, 'webp', optimize = True, quality = 100)
-        print(f"Saved as: {new_filename}")
+with Image.open("home - cecil hunter commit.webp") as img:
+    # Save with '_rotated' suffix
+    new_filename = f"home - cecil hunter commit - small - 90.webp"
+    img.thumbnail([1000,1600],Image.LANCZOS)
+    img.save(new_filename, 'webp', optimize = True, quality = 90)
+    print(f"Saved as: {new_filename}")
